278b279464a00ffbc8395fbc543f98ee055a374d
Robert Lowe <robertmlowe@rmlowe.com>
2022-01-15 17:04:05 +0000

    Updating to work with new versin of https://leetcode.com/problemset/all/

--- crx/content.js
+++ crx/content.js
@@ -1,19 +1,27 @@
 (() => {
-    const companies = {};
+  const companies = {};
 
-    for (const a of document.querySelectorAll('.lg-company,.sm-company')) {
-        companies[a.href.split('/')[4]] = {
-            Company: a.querySelector('.text-gray').textContent.trim(),
-            'All time': parseInt(a.querySelector('.badge').textContent, 10)
-        };
-    }
+  // For old version
+  for (const a of document.querySelectorAll('.lg-company,.sm-company')) {
+    companies[a.href.split('/')[4]] = {
+      Company: a.querySelector('.text-gray').textContent.trim(),
+      'All time': parseInt(a.querySelector('.badge').textContent, 10)
+    };
+  }
 
-    const csrftoken = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('csrftoken'))
-        .split('=')[1];
+  // // For new version
+  // for (const a of document.querySelectorAll('.swiper-slide a.mb-4')) {
+  //   companies[a.href.split('/')[4]] = {};
+  // }
 
-    const query = `query getCompanyTag($slug: String!) {
+  // console.log(companies);
+
+  const csrftoken = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('csrftoken'))
+    .split('=')[1];
+
+  const query = `query getCompanyTag($slug: String!) {
   companyTag(slug: $slug) {
     name
     translatedName
@@ -75,65 +83,65 @@ fragment questionFields on QuestionNode {
 }
 `;
 
-    const compareFunction = (c1, c2) => {
-        for (const key of ['6 months', '1 year', '2 years', 'All time']) {
-            const v1 = c1[key];
-            const v2 = c2[key];
+  const compareFunction = (c1, c2) => {
+    for (const key of ['6 months', '1 year', '2 years', 'All time']) {
+      const v1 = c1[key];
+      const v2 = c2[key];
 
-            if (v1 !== undefined && v2 !== undefined) {
-                const diff = v2 - v1;
+      if (v1 !== undefined && v2 !== undefined) {
+        const diff = v2 - v1;
 
-                if (diff !== 0) {
-                    return diff;
-                }
-            }
+        if (diff !== 0) {
+          return diff;
         }
-
-        return 0;
+      }
     }
 
-    const sendUpdate = () => {
-        const message = Object.values(companies);
-        message.sort(compareFunction);
-        chrome.runtime.sendMessage(message);
+    return 0;
+  }
+
+  const sendUpdate = () => {
+    const message = Object.values(companies);
+    message.sort(compareFunction);
+    chrome.runtime.sendMessage(message);
+  };
+
+  async function updateProblemCounts(slug) {
+    const value = {
+      operationName: 'getCompanyTag',
+      query,
+      variables: {
+        slug
+      }
     };
 
-    async function updateProblemCounts(slug) {
-        const value = {
-            operationName: 'getCompanyTag',
-            query,
-            variables: {
-                slug
-            }
-        };
+    const { data } = await fetch('https://leetcode.com/graphql', {
+      method: 'post',
+      headers: {
+        "content-type": "application/json",
+        "x-csrftoken": csrftoken
+      },
+      body: JSON.stringify(value)
+    })
+      .then(response => response.json());
 
-        const { data } = await fetch('https://leetcode.com/graphql', {
-            method: 'post',
-            headers: {
-                "content-type": "application/json",
-                "x-csrftoken": csrftoken
-            },
-            body: JSON.stringify(value)
-        })
-            .then(response => response.json());
+    const questionCountByFrequencyTimePeriod = frequencyTimePeriod =>
+      data.companyTag.questions.filter(question => question.frequencyTimePeriod <= frequencyTimePeriod).length;
 
-        const questionCountByFrequencyTimePeriod = frequencyTimePeriod =>
-            data.companyTag.questions.filter(question => question.frequencyTimePeriod <= frequencyTimePeriod).length;
-
-        companies[slug] = {
-            Company: data.companyTag.name,
-            '6 months': questionCountByFrequencyTimePeriod(1),
-            '1 year': questionCountByFrequencyTimePeriod(2),
-            '2 years': questionCountByFrequencyTimePeriod(3),
-            'All time': questionCountByFrequencyTimePeriod(4)
-        };
-
-        sendUpdate();
-    }
+    companies[slug] = {
+      Company: data.companyTag.name,
+      '6 months': questionCountByFrequencyTimePeriod(1),
+      '1 year': questionCountByFrequencyTimePeriod(2),
+      '2 years': questionCountByFrequencyTimePeriod(3),
+      'All time': questionCountByFrequencyTimePeriod(4)
+    };
 
     sendUpdate();
+  }
 
-    for (const slug of Object.keys(companies)) {
-        updateProblemCounts(slug);
-    }
+  sendUpdate();
+
+  for (const slug of Object.keys(companies)) {
+    updateProblemCounts(slug);
+  }
 })();
